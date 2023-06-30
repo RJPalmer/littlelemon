a908f88c44300ad3d3f31dae15d6544410f3725f
Robert Palmer <Awriter247@gmail.com>
2023-06-30 10:58:53 -0400

    correct starting project folder

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Robert Palmer on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
